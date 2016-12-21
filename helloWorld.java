97aa39a59cf6d098593c07dd65221f3d8161a39f
ilay92 <ilay@Ilays-MacBook-Pro-2.local>
2016-12-21 11:11:04 +0200

    Initial commit

---
+++ helloWorld.java
@@ -0,0 +1,12 @@
+/**
+ * A standard hello-world program 
+ * @author Ilay
+ *
+ */
+public class helloWorld {
+	
+	public static void main (String[] args){
+		System.out.println("Hello world");
+	}
+	
+}
